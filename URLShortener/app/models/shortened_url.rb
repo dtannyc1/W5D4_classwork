231f4598cdcdf68ba0af56817be6720e1f7dd12e
dtannyc1 <dtannyc1@gmail.com>
2023-04-06 16:42:05 -0400

    Started ShortenedUrl::random_code

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -9,7 +9,24 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+
+require 'securerandom'
+
 class ShortenedUrl < ApplicationRecord
     validates :short_url, uniqueness: true, presence: true
     validates :user_id, presence: true
+
+    def self.random_code
+        str = SecureRandom.urlsafe_base64
+        self.exists?(:short_url => str)
+    end
+
+    after_initialize do |shortenedurl|
+        generate_short_url
+    end
+
+    private
+    def generate_short_url
+
+    end
 end
