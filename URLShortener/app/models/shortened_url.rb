04850c120dde73c1cda8f8c59577ef0da8949100
lewiskk016 <lewiskk016@gmail.com>
2023-04-06 16:21:48 -0400

    short url validation

---
+++ URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id         :bigint           not null, primary key
+#  long_url   :string
+#  short_url  :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ShortenedUrl < ApplicationRecord
+    validates :short_url, uniqueness: true, presence: true
+    validates :user_id, presence: true
+end
